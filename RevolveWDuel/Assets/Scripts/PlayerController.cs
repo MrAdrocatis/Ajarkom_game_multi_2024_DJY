e701ff35d615a2411f23e14fae46a0d3da875c36
devina yusra <yw.devina@gmail.com>
2024-10-23 20:46:15 +0700

    updateControl
    + bullet and weapon
    + player aims at cursor
    - FSM erased

---
+++ RevolveWDuel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Weapon weapon;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+        }
+
+        // Update the movement direction based on WASD keys
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        // Get the mouse position in world space
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the player based on input
+        rb.velocity = moveDirection * moveSpeed;
+
+        // Calculate the direction to aim at based on the mouse position
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+
+        // Set the rotation based on the aim direction
+        rb.rotation = aimAngle;
+    }
+}
