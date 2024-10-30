c6f0c623275bdbcc37c8dd259faf3cdff29eabb6
MrAdrocatis <mr.adrocatis@gmail.com>
2024-10-30 19:06:18 +0700

    Update 1.2
    Balikin model untuk character di scene move.

--- RevolveWDuel/Assets/Scripts/UI_Script/AmmoDisplay.cs
+++ RevolveWDuel/Assets/Scripts/UI_Script/AmmoDisplay.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AmmoDisplay : MonoBehaviour
 {
+    public int ammo;
+    public Text ammoDisplay; 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class AmmoDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ammoDisplay.text = ammo.ToString();
     }
 }
